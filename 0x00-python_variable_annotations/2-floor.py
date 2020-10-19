befe799c616eeeed9c8ec11d606af74997d7a417
agzsoftsi <agzsoftsi@gmail.com>
2020-10-19 23:23:01 +0000

    Task 2 done

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Description:takes a float n as argument and returns the floor of the float
+    Arguments: n: float
+'''
+
+
+def floor(n: float) -> int:
+    ''' Return largest int value less than or equal to n. '''
+    return int(n) if n >= 0 else int(n) - 1
