3a8f7c9fe96b483a11462dddac7f73dbfa5eea7d
agzsoftsi <agzsoftsi@gmail.com>
2020-10-20 16:49:20 +0000

    Task 4 Done

--- 0x01-python_async_function/4-main.py
+++ 0x01-python_async_function/4-main.py
@@ -4,6 +4,6 @@ import asyncio
 
 task_wait_n = __import__('4-tasks').task_wait_n
 
-        n = 5
-        max_delay = 6
-        print(asyncio.run(task_wait_n(n, max_delay)))
+n = 5
+max_delay = 6
+print(asyncio.run(task_wait_n(n, max_delay)))
