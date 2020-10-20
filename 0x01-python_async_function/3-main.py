e1e15d96bfdb461dbbe6276ddf6b339332258f41
agzsoftsi <agzsoftsi@gmail.com>
2020-10-20 16:37:13 +0000

    Task 3 Done

--- 0x01-python_async_function/3-main.py
+++ 0x01-python_async_function/3-main.py
@@ -6,8 +6,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def test(max_delay: int) -> float:
-        task = task_wait_random(max_delay)
-            await task
-                print(task.__class__)
+    task = task_wait_random(max_delay)
+    await task
+    print(task.__class__)
 
-                asyncio.run(test(5))
+asyncio.run(test(5))
