c142f58499b5e4eb241af5751079c9c2049e0b37
agzsoftsi <agzsoftsi@gmail.com>
2020-10-20 00:02:06 +0000

    Task 4 done

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+''' Description: Define and annotate the following variables with the
+    specified values
+    Arguments:  a - integer with value 1
+                pi - float with value 3.14
+                i_understand_annotations - boolean with value True
+                school - str with value 'Holberton'
+'''
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
