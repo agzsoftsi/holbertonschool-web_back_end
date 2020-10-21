e5f08bc506d621d6301f7ec4bcf15a37ab28aa14
agzsoftsi <agzsoftsi@gmail.com>
2020-10-21 16:38:16 +0000

    Task 1 Done

--- 0x02-python_async_comprehension/1-main.py
+++ 0x02-python_async_comprehension/1-main.py
@@ -8,4 +8,4 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def main():
     print(await async_comprehension())
 
-    asyncio.run(main())
+asyncio.run(main())
