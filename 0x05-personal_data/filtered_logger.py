d35161937d8d0db8508eededb21ca87445bfe84d
agzsoftsi <agzsoftsi@gmail.com>
2020-11-02 17:50:42 +0000

    Task 2 - fix Global data

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -9,7 +9,7 @@ from typing import List
 import logging
 
 
-PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
