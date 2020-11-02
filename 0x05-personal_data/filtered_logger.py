056b0f1433b363cd85eab6ddb6b766090a743a84
agzsoftsi <agzsoftsi@gmail.com>
2020-11-02 17:23:15 +0000

    Task 1 fix Fields Argument

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -49,7 +49,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
