d3a66bb5fdf31bd4e82d7dbcb28e163ce653912c
agzsoftsi <agzsoftsi@gmail.com>
2020-11-02 17:08:22 +0000

    Task 1 - only base

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-''' 0. Regex-ing '''
+''' 0. Regex-ing: filter_datum
+    1. Log formatter: class RedactingFormatter
+'''
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -28,3 +31,17 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                          i + "=" + redaction + separator,
                          message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
