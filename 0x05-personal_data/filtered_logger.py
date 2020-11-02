240bb8c28b74102472913a7ae09ecec3eab6c16b
agzsoftsi <agzsoftsi@gmail.com>
2020-11-02 16:53:53 +0000

    Task 0 Adjust Comment description for this task

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
-''' Description: Regex-ing - Write a function called filter_datum that returns
-                 the log message obfuscated:
-
-    Arguments:
-                fields: a list of strings representing all fields to obfuscate
-                redaction: a string representing by what the field will be
-                           obfuscated
-                message: a string representing the log line
-                separator: a string representing by which character is
-                           separating all fields in the log line (message)
-   The function should use a regex to replace occurrences of certain field
-   values.
-   filter_datum should be less than 5 lines long and use re.sub to perform
-   the substitution with a single regex.
-'''
+''' 0. Regex-ing '''
 
 import re
 from typing import List
@@ -21,7 +7,21 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    ''' Return `message` with personally identifiable information redacted. '''
+    ''' Description: Regex-ing - Write a function called filter_datum that
+                     returns the log message obfuscated:
+
+        Arguments:
+            fields: a list of strings representing all fields to obfuscate
+            redaction: a string representing by what the field will be
+                       obfuscated
+            message: a string representing the log line
+            separator: a string representing by which character is
+                           separating all fields in the log line (message)
+        The function should use a regex to replace occurrences of certain
+        field values.
+        filter_datum should be less than 5 lines long and use re.sub to
+        perform the substitution with a single regex.
+    '''
 
     for i in fields:
         message = re.sub(i + "=.*?" + separator,
