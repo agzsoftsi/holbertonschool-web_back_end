ea7bfab5253469027f12e15aa4b914f3083aa5d6
agzsoftsi <agzsoftsi@gmail.com>
2020-11-03 00:00:22 +0000

    Task 6 Done

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''
    5. Encrypting passwords
+   6. Check valid password
 '''
 import bcrypt
 
@@ -16,3 +17,20 @@ def hash_password(password: str) -> bytes:
     pass_hashed = bcrypt.hashpw(pass_encoded, bcrypt.gensalt())
 
     return pass_hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''
+       Description: Implement an is_valid function that expects 2 arguments and
+                    returns a boolean.
+
+       Arguments:   hashed_password: bytes type
+                    password: string type
+       Use bcrypt to validate that the provided password matches the hashed
+       password.
+    '''
+    valid = False
+    pass_encoded = password.encode()
+    if bcrypt.checkpw(pass_encoded, hashed_password):
+        valid = True
+    return valid
