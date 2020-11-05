18e575880b2ee4c8c5f0a992bd3ab68938955f73
agzsoftsi <agzsoftsi@gmail.com>
2020-11-05 17:15:34 +0000

    Task 1 fixed add Executables and pycodestyle

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ GET /api/v1/unauthorized
