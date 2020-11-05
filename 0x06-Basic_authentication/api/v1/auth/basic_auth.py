7119674c643b407b89f031230317a468640495ba
agzsoftsi <agzsoftsi@gmail.com>
2020-11-05 22:47:25 +0000

    Task 8 done

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of Basic Authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,19 @@ class BasicAuth(Auth):
         encoded = authorization_header.split(' ', 1)[1]
 
         return encoded
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ Decodes the value of a base64 string """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+        except base64.binascii.Error:
+            return None
+
+        return decoded.decode('utf-8')
