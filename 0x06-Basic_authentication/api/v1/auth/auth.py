1515f5f6fa2f2c79af02b432505dc881dc5a2623
agzsoftsi <agzsoftsi@gmail.com>
2020-11-05 22:14:43 +0000

    Fix  Define which routes don't need authentication on auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -10,23 +10,15 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method for requiring authentication """
-        if path is None or excluded_paths is None or excluded_paths == []:
+        if path is None or excluded_paths is None or not len(excluded_paths):
             return True
-
-        l_path = len(path)
-        slash_path = True if path[l_path - 1] == '/' else False
-        for exc in excluded_paths:
-            l_exc = len(exc)
-            slash_exc = True if exc[l_exc - 1] == '/' else False
-
-            if slash_path and not slash_exc:
-                path = path[:-1]
-            elif not slash_path and slash_exc:
-                path += '/'
-
-            if path == exc:
-                return False
-
+        # Add slash to all cases for consistency
+        if path[-1] != '/':
+            path += '/'
+        if excluded_paths[-1] != '/':
+            excluded_paths += '/'
+        if path in excluded_paths:
+            return False
         return True
 
     def authorization_header(self, request=None) -> str:
