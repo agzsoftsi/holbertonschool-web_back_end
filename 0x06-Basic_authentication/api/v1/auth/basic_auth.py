f68f5e8b1cd612162457f375702853b89fc4f676
agzsoftsi <agzsoftsi@gmail.com>
2020-11-06 15:58:23 +0000

    Task 11 Done

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@
 """ Module of Basic Authentication
 """
 from api.v1.auth.auth import Auth
-import base64
+from base64 import b64decode
 from models.user import User
 from typing import TypeVar
 
@@ -36,11 +36,13 @@ class BasicAuth(Auth):
             return None
 
         try:
-            decoded = base64.b64decode(base64_authorization_header)
-        except base64.binascii.Error:
+            encoded = base64_authorization_header.encode('utf-8')
+            decoded64 = b64decode(encoded)
+            decoded = decoded64.decode('utf-8')
+        except BaseException:
             return None
 
-        return decoded.decode('utf-8')
+        return decoded
 
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header: str
@@ -86,3 +88,29 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ overloads Auth and retrieves the User instance for a request """
+        auth_header = self.authorization_header(request)
+
+        if not auth_header:
+            return None
+
+        encoded = self.extract_base64_authorization_header(auth_header)
+
+        if not encoded:
+            return None
+
+        decoded = self.decode_base64_authorization_header(encoded)
+
+        if not decoded:
+            return None
+
+        email, pwd = self.extract_user_credentials(decoded)
+
+        if not email or not pwd:
+            return None
+
+        user = self.user_object_from_credentials(email, pwd)
+
+        return user
