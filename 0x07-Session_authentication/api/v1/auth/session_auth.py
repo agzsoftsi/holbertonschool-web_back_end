2f66b5e80538b3ce47fe898bfba07bdfff7a69b3
agzsoftsi <agzsoftsi@gmail.com>
2020-11-10 18:23:50 +0000

    session authentication with id for task 2

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,19 @@
 ''' Define SessionAuth class. '''
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     ''' Extend behavior of Auth class for session authentication. '''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        ''' Create and return a session ID for a user ID. '''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+
+        return session_id
