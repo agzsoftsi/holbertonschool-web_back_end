1d3449b2159267540e3d4a86edad285a1d0fc8c6
agzsoftsi <agzsoftsi@gmail.com>
2020-11-10 18:28:04 +0000

    session authentication with user id and session id for task 3

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -18,3 +18,10 @@ class SessionAuth(Auth):
         SessionAuth.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        ''' Return user ID associated with specified session ID. '''
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return SessionAuth.user_id_by_session_id.get(session_id)
