838dd80e300b08af097fa31f853a366a5e070f23
agzsoftsi <agzsoftsi@gmail.com>
2020-11-10 18:10:42 +0000

    add endpoint for task 0 fix

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -68,6 +68,7 @@ def before_request() -> str:
 
     request.current_user = auth.current_user(request)
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
--- 0x07-Session_authentication/api/v1/views/users.py
+++ 0x07-Session_authentication/api/v1/views/users.py
@@ -25,8 +25,10 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
+
     if user_id is None:
         abort(404)
+
     if user_id == "me" and request.current_user is None:
         abort(404)
 
@@ -36,6 +38,7 @@ def view_one_user(user_id: str = None) -> str:
     user = User.get(user_id)
     if user is None:
         abort(404)
+
     return jsonify(user.to_json())
 
 
