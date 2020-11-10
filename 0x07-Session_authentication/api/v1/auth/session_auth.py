05cda26c35372c301bfa68d7ea65fb253840cfa1
agzsoftsi <agzsoftsi@gmail.com>
2020-11-10 18:58:41 +0000

    session authentication - log out for task 8

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -39,3 +39,22 @@ class SessionAuth(Auth):
         print(user_id)
 
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Deletes de user session / logout"""
+
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if not user_id:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+
+        return True
--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -46,3 +46,20 @@ def login():
     response.set_cookie(SESSION_NAME, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """ DELETE /auth_session/logout
+    Return:
+        - Empty dictionary if succesful
+    """
+    from api.v1.app import auth
+
+    deleted = auth.destroy_session(request)
+
+    if not deleted:
+        abort(404)
+
+    return jsonify({}), 200
