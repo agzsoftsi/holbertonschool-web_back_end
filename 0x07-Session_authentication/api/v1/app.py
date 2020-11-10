6353365ef087779fb3ee71234d9a77ffe88ef25a
agzsoftsi <agzsoftsi@gmail.com>
2020-11-10 18:05:13 +0000

    add endpoint for task 0 fix

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -65,6 +65,7 @@ def before_request() -> str:
 
     if auth.current_user(request) is None:
         abort(403)
+
     request.current_user = auth.current_user(request)
 
 if __name__ == "__main__":
--- 0x07-Session_authentication/api/v1/views/users.py
+++ 0x07-Session_authentication/api/v1/views/users.py
@@ -32,6 +32,7 @@ def view_one_user(user_id: str = None) -> str:
 
     if user_id == "me" and request.current_user is not None:
         return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
