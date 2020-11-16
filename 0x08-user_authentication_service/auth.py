e075a74fb8ffadebe5b70235878e094248b357e8
agzsoftsi <agzsoftsi@gmail.com>
2020-11-16 17:58:57 +0000

    Task 4 - Hash password fix

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 """ Authentication Module """
 
-import bcrypt
+from bcrypt import hashpw, gensalt
 
 
 def _hash_password(password: str) -> str:
     """ Returns a salted hash of the input password """
-    encoded = password.encode()
-    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
-
-    return str(hashed)
+    return hashpw(password.encode('utf-8'), gensalt())
