561a06ac6ab5c0fecd94d1041795dd4377d0cce0
agzsoftsi <agzsoftsi@gmail.com>
2020-11-16 17:38:02 +0000

    Task 2 - Find user fix

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,11 +4,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from typing import TypeVar
 from user import Base
 from user import User
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
 
 
 class DB:
