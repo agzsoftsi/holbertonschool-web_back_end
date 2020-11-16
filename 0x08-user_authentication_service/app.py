25eb1c9d48c58e0a077787ba246b8e6fe1d6edab
agzsoftsi <agzsoftsi@gmail.com>
2020-11-16 19:14:34 +0000

    Task 14 - Log out

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import abort
+from flask import redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -64,6 +65,27 @@ def login() -> str:
             response.set_cookie('session_id', session_id)
 
             return response
+
+
+@app.route('/sessions', methods=['DELETE'])
+def log_out() -> str:
+    """Find the user with the requested session ID.
+    If the user exists destroy the session and redirect the user to GET /.
+    If the user does not exist, respond with a 403 HTTP status.
+    """
+    session_id = request.cookies.get("session_id", None)
+
+    if session_id is None:
+        abort(400)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+
+    return redirect('/')
 
 
 if __name__ == "__main__":
