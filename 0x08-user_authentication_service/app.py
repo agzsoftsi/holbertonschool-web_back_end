46410eb628496d22235e74126dfd5ef3a1d5d71f
agzsoftsi <agzsoftsi@gmail.com>
2020-11-16 18:19:18 +0000

    Task 7 - Register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import abort
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def hello_world() -> str:
     msg = {"message": "Bienvenue"}
+    return jsonify(msg)
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
     return jsonify(msg)
 
 
