84b5a67ec7980e468db824e18fea886f2ad27be3
agzsoftsi <agzsoftsi@gmail.com>
2020-11-16 17:16:20 +0000

    Task 1 - create user

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Database for ORM """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """ DB Class for Object Reational Mappin """
+
+    def __init__(self):
+        """ Constructor Method """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """ Session Getter Method """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """ Adds user to database
+        Return: User Object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+
+        return user
