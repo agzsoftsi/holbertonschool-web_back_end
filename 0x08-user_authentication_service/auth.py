4c4c68731179fa16f6d7ff8257ae5be4aff36e20
agzsoftsi <agzsoftsi@gmail.com>
2020-11-17 04:33:39 +0000

    Task 18 -  Update password fix

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -92,17 +92,16 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Uses reset token to validate update of users password"""
-        try:
-            user = self._db.find_user_by(reset_token=reset_token)
-        except NoResultFound:
-            raise ValueError
-
-        hashed_password = _hash_password(password)
-        self._db.update_user(user.id,
-                             hashed_password=hashed_password,
-                             reset_token=None)
-
-        return None
+        if reset_token and password:
+            try:
+                user = self._db.find_user_by(reset_token=reset_token)
+                if user:
+                    hashed_password = _hash_password(password)
+                    self._db.update_user(user.id,
+                                         hashed_password=hashed_password,
+                                         reset_token=None)
+            except Exception:
+                raise ValueError
 
 
 def _generate_uuid() -> str:
