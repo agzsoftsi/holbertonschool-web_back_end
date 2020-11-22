c139414f839bfab57fa9dc4696a4c23dce6ed8b8
agzsoftsi <agzsoftsi@gmail.com>
2020-11-22 21:34:08 +0000

    Task 2 - Mock HTTP calls

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,11 @@
 """ Test SUITE Unittest module Task """
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 
 from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,3 +30,23 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as e:
             access_nested_map(map, path)
             self.assertEqual(wrong_output, e.exception)
+
+
+class TestGetJson(unittest.TestCase):
+    """ Class for testing get_json function """
+    # order of args: test_url, test_payload
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Test method returns correct output """
+        # set mock response to have return value of test payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        # function calls requests.get, need patch to get mock return value
+        with patch('requests.get', return_value=mock_response):
+            real_response = get_json(test_url)
+            self.assertEqual(real_response, test_payload)
+            # check that mocked method called once per input
+            mock_response.json.assert_called_once()
