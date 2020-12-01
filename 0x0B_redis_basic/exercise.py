b7c247c81df513c3efd1be5efa81585f5ac22eca
agzsoftsi <agzsoftsi@gmail.com>
2020-12-01 16:55:32 +0000

    Add Task 0 with Class fix uuid4

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -17,5 +17,5 @@ class Cache:
         """Stores input data in Redis using a random key
         """
         key = str(uuid4())
-        self._redis.set(key: data)
+        self._redis.set(key, data)
         return key
