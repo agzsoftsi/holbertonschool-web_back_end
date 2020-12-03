6e436b3488e9dc29e9c05aafbab847f52e25b82f
Carlos Garcia <agzsoftsi@gmail.com>
2020-12-03 09:07:53 -0500

    Task 6. Add bonus

---
+++ 0x0C-MySQL_Advanced/6-main.sql
@@ -0,0 +1,27 @@
+Enter password: 
+id  name
+1   C is fun
+2   Python is cool
+user_id project_id  score
+1   1   80
+1   2   96
+2   1   91
+2   2   73
+--
+--
+--
+--
+id  name
+1   C is fun
+2   Python is cool
+3   Bonus project
+4   New bonus
+user_id project_id  score
+1   1   80
+1   2   96
+2   1   91
+2   2   73
+2   2   100
+2   3   100
+1   3   10
+2   4   90
