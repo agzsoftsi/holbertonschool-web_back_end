14b4b76ff3d7c62176f814de4440f0b6cdddcd9e
Carlos Garcia <agzsoftsi@gmail.com>
2020-12-03 08:54:40 -0500

    Task 2. Best band ever!

---
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+-- Task 2: 2. Best band ever! - ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+SELECT DISTINCT `origin`, SUM(`fans`) as `nb_fans` FROM `metal_bands`
+GROUP BY `origin`
+ORDER BY `nb_fans` DESC;
