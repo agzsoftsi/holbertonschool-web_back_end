e1e342a97e2603317ffcba624c7393d066b2d190
Carlos Garcia <agzsoftsi@gmail.com>
2020-12-04 15:29:08 -0500

    Update 101-average_weighted_score.sql

--- 0x0C-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,2 +1,18 @@
 -- Task 13 - Average weighted score for all! -  computes and store the average weighted score for all students.
 -- Procedure ComputeAverageWeightedScoreForUsers is not taking any input.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+    UPDATE users AS U, 
+        (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
+        FROM users AS U 
+        JOIN corrections as C ON U.id=C.user_id 
+        JOIN projects AS P ON C.project_id=P.id 
+        GROUP BY U.id)
+    AS WA
+    SET U.average_score = WA.w_avg 
+    WHERE U.id=WA.id;
+END
+$$
+DELIMITER ;
