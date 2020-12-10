4ba534f5b46a4185327ade41ce7e7a63f4fe723c
agzsoftsi <agzsoftsi@gmail.com>
2020-12-10 20:34:12 +0000

    0. Const or let?

--- 0x0E-ES6_basic/0-constants.js
+++ 0x0E-ES6_basic/0-constants.js
@@ -1,5 +1,3 @@
-// Task 0 - function taskFirst to instantiate variables using const
-//        - function taskNext to instantiate variables using let
 export function taskFirst() {
 	  const task = 'I prefer const when I can.';
 	  return task;
