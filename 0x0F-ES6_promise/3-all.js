5b5ab3e237b9b9296191a600a800f0aaeffb14be
agzsoftsi <agzsoftsi@gmail.com>
2020-12-15 20:46:14 +0000

    Task 3

---
+++ 0x0F-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
+
+export default handleProfileSignup;
