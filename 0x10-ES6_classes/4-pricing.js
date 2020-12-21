8b172fdcafb92d6fe74db8743f72c8c75e1aab0e
agzsoftsi <agzsoftsi@gmail.com>
2020-12-21 17:27:30 +0000

    TAsk 4 done

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
