d5b6dc274c7b26310b05647c7c565e290dbde339
agzsoftsi <agzsoftsi@gmail.com>
2020-12-21 17:44:13 +0000

    TAsk 10 done

---
+++ 0x10-ES6_classes/10-car.js
@@ -0,0 +1,18 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+}
+
+export default Car;
