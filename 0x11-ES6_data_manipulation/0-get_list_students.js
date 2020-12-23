3ae22d6240c9be3b1e5df366bc051153390d6db4
Carlos Garcia <agzsoftsi@gmail.com>
2020-12-23 09:07:49 -0500

    Task 0

---
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+function getListStudents() {
+  const obj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+
+  return obj;
+}
+
+export default getListStudents;
