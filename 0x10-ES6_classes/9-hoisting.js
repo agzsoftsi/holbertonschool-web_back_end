6c8ab408e499fb7b2ef4a2e9108d7b755f124caf
agzsoftsi <agzsoftsi@gmail.com>
2020-12-23 13:30:25 +0000

    Fix task 9 - add fullstudentdESCRIPTION

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -30,6 +30,10 @@ export class StudentHolberton {
   get holbertonClass() {
     return this._holbertonClass;
   }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
 }
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
