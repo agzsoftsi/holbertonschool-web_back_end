552e2aaefd334ecff9558162ba337819c7c83646
Carlos Garcia <agzsoftsi@gmail.com>
2020-12-23 09:08:25 -0500

    TAsk 2

---
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,11 @@
+function getStudentsByLocation(students, city) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const result = students.filter((item) => item.location === city);
+
+  return result;
+}
+
+export default getStudentsByLocation;
