8fff7e36e6314961d740b29306e0255e089b4979
Carlos Garcia <agzsoftsi@gmail.com>
2021-01-13 19:13:06 -0500

    Fix test Task 1

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -1,75 +1,110 @@
-const calculateNumber = require("./1-calcul.js");
-const assert = require("assert");
+const assert = require('assert');
+const mocha = require('mocha');
 
-describe('SUM', () => {
-  it('returns rounded positive sum', () => {
-    assert.strictEqual(calculateNumber('SUM', 6.1, 6.1), 12);
-    assert.strictEqual(calculateNumber('SUM', 0.1, 0.2), 0);
-    assert.strictEqual(calculateNumber('SUM', 0.1, 0.6), 1);
-    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-    assert.strictEqual(calculateNumber('SUM', 4, 8), 12);
-    assert.strictEqual(calculateNumber('SUM', 1.9, 0), 2);
-   
+const calculateNumber = require('./1-calcul');
+
+const SUM = 'SUM';
+const SUBTRACT = 'SUBTRACT';
+const DIVIDE = 'DIVIDE';
+const INVALID = 'INVALID';
+
+describe('calculateNumber', () => {
+  describe('type SUM', () => {
+    it('should return sum of integers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+      assert.strictEqual(calculateNumber(SUM, 2, -2), 0);
+      assert.strictEqual(calculateNumber(SUM, 1, -4), -3);
+    });
+
+    it('should return sum of rounded floats', () => {
+      assert.strictEqual(calculateNumber(SUM, 1.4, 5), 6);
+      assert.strictEqual(calculateNumber(SUM, 1, 4.5), 6);
+      assert.strictEqual(calculateNumber(SUM, 1.4, 4.5), 6);
+      assert.strictEqual(calculateNumber(SUM, 8, -8.2), 0);
+      assert.strictEqual(calculateNumber(SUM, 7.8, -8), 0);
+      assert.strictEqual(calculateNumber(SUM, 7.8, -8.2), 0);
+      assert.strictEqual(calculateNumber(SUM, 3, -5.2), -2);
+      assert.strictEqual(calculateNumber(SUM, 3.1, -5), -2);
+      assert.strictEqual(calculateNumber(SUM, 3.1, -5.2), -2);
+    });
+
+    it('should return rounded number if only one is provided', () => {
+      assert.strictEqual(calculateNumber(SUM, 8.7), 9);
+      assert.strictEqual(calculateNumber(SUM, 0.3), 0);
+      assert.strictEqual(calculateNumber(SUM, -8.7), -9);
+    });
   });
 
-  it('returns rounded negative sum', () => {
-    assert.strictEqual(calculateNumber('SUM', -1, -3), -4);
-    assert.strictEqual(calculateNumber('SUM', -1.4, -3.6), -5);
+  describe('type SUBTRACT', () => {
+    it('should return difference of integers', () => {
+      assert.strictEqual(calculateNumber(SUBTRACT, 5, 1), 4);
+      assert.strictEqual(calculateNumber(SUBTRACT, 5, 5), 0);
+      assert.strictEqual(calculateNumber(SUBTRACT, 1, 5), -4);
+    });
+
+    it('should return difference of rounded floats', () => {
+      assert.strictEqual(calculateNumber(SUBTRACT, 8.6, 4.1), 5);
+      assert.strictEqual(calculateNumber(SUBTRACT, 8.6, 4), 5);
+      assert.strictEqual(calculateNumber(SUBTRACT, 9, 4.1), 5);
+
+      assert.strictEqual(calculateNumber(SUBTRACT, 10.2, 9.8), 0);
+      assert.strictEqual(calculateNumber(SUBTRACT, 10.2, 10), 0);
+      assert.strictEqual(calculateNumber(SUBTRACT, 10, 9.8), 0);
+
+      assert.strictEqual(calculateNumber(SUBTRACT, 8.2, 9.2), -1);
+      assert.strictEqual(calculateNumber(SUBTRACT, 8.2, 9), -1);
+      assert.strictEqual(calculateNumber(SUBTRACT, 8, 9.2), -1);
+    });
+
+    it('should return rounded number if only one is provided', () => {
+      assert.strictEqual(calculateNumber(SUBTRACT, 8.7), 9);
+      assert.strictEqual(calculateNumber(SUBTRACT, 0.3), 0);
+      assert.strictEqual(calculateNumber(SUBTRACT, -8.7), -9);
+    });
   });
 
-  it('returns TypeError', () => {
-    assert.throws(() => calculateNumber('SUM', NaN, NaN), { name: 'TypeError' });
-    assert.throws(() => calculateNumber('SUM', NaN, 5.6), { name: 'TypeError' });
-    assert.throws(() => calculateNumber('SUM', 5.6, NaN), { name: 'TypeError' });
-  });
-});
+  describe('type DIVIDE', () => {
+    it('should return quotient of integers', () => {
+      assert.strictEqual(calculateNumber(DIVIDE, 9, 4), 2.25);
+      assert.strictEqual(calculateNumber(DIVIDE, -9, 4), -2.25);
+      assert.strictEqual(calculateNumber(DIVIDE, 9, -4), -2.25);
+      assert.strictEqual(calculateNumber(DIVIDE, -9, -4), 2.25);
+    });
 
-describe('SUBTRACT', () => {
-  it('returns rounded positive substract', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 0.6, 0.1), 1);
-    assert.strictEqual(calculateNumber('SUBTRACT', 4.5, 1.4), 4);
-    assert.strictEqual(calculateNumber('SUBTRACT', 8, 4), 4);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1.9, 0), 2);
-    assert.strictEqual(calculateNumber('SUBTRACT', 6.1, 6.1), 0);
-    assert.strictEqual(calculateNumber('SUBTRACT', 1, 0.2), 1);
-  });
+    it('should return quotient of non-zero rounded floats', () => {
+      assert.strictEqual(calculateNumber(DIVIDE, 1.6, 5.2), 0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, 1.6, 5), 0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, 2, 5.2), 0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, -1.6, 5.2), -0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, -1.6, 5), -0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, -2, 5.2), -0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, 1.6, -5.2), -0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, 1.6, -5), -0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, 2, -5.2), -0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, -1.6, -5.2), 0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, -1.6, -5), 0.4);
+      assert.strictEqual(calculateNumber(DIVIDE, -2, -5.2), 0.4);
+    });
 
-  it('returns rounded negative substract', () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', -1, -3), 2);
-    assert.strictEqual(calculateNumber('SUBTRACT', -1.4, -3.6), 3);
-  });
+    it('should return 0 if dividend rounds to 0', () => {
+      assert.strictEqual(calculateNumber(DIVIDE, 0.3, 3.6), 0);
+      assert.strictEqual(calculateNumber(DIVIDE, -0.3, 3.6), 0);
+      assert.strictEqual(calculateNumber(DIVIDE, 0, 3.6), 0);
+    });
 
-  it('returns TypeError', () => {
-    assert.throws(() => calculateNumber('SUBTRACT', NaN, 5.6), { name: 'TypeError' });
-    assert.throws(() => calculateNumber('SUBTRACT', 5.6, NaN), { name: 'TypeError' });
-    assert.throws(() => calculateNumber('SUBTRACT', NaN, NaN), { name: 'TypeError' });
-  });
-});
-
-describe('DIVIDE', () => {
-  it('returns rounded positive divide', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 0.6, 0.9), 1);
-    assert.strictEqual(calculateNumber('DIVIDE', 8, 4), 2);
-    assert.strictEqual(calculateNumber('DIVIDE', 6.1, 1.7), 3);
-    assert.strictEqual(calculateNumber('DIVIDE', 6.1, 6.1), 1);
-    assert.strictEqual(calculateNumber('DIVIDE', 2.0, 1.1), 2);
-    assert.strictEqual(calculateNumber('DIVIDE', 4.5, 5), 1);
+    it('should return "ERROR" if divisor rounds to 0', () => {
+      assert.strictEqual(calculateNumber(DIVIDE, 8.4, 0.4), 'ERROR');
+      assert.strictEqual(calculateNumber(DIVIDE, 8.4, -0.4), 'ERROR');
+      assert.strictEqual(calculateNumber(DIVIDE, 8.4, 0), 'ERROR');
+    });
   });
 
-  it('returns rounded negative divide', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', -2, 1.1), -2);
-    assert.strictEqual(calculateNumber('DIVIDE', -4.4, -2.2), 2);
-  });
-
-  it('returns Error', () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 2, 0), 'Error');
-    assert.strictEqual(calculateNumber('DIVIDE', 3, 0), 'Error');
-    assert.strictEqual(calculateNumber('DIVIDE', 4, 0), 'Error');
-  });
-
-  it('returns TypeError', () => {
-    assert.throws(() => calculateNumber('DIVIDE', NaN, 5.6), { name: 'TypeError' });
-    assert.throws(() => calculateNumber('DIVIDE', 5.6, NaN), { name: 'TypeError' });
-    assert.throws(() => calculateNumber('DIVIDE', NaN, NaN), { name: 'TypeError' });
+  describe('invalid operation type', () => {
+    it('should throw error if type is invalid', () => {
+      assert.throws(() => calculateNumber(INVALID, 5.7, 4.6), {
+        message:
+          'Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".'
+      });
+    });
   });
 });
